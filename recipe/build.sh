64713617627c9d63d12f9e15ea30d1aa312c50c9
Matthew R. Becker <beckermr@users.noreply.github.com>
2020-03-12 07:04:49 -0500

    BUG do not vendor (#15)
    * BUG do not vendor
    
    * Update meta.yaml

--- recipe/build.sh
+++ recipe/build.sh
@@ -11,6 +11,10 @@ fi
 make
 make install
 
+mkdir -p _deps/lib
+ln -s ${PREFIX}/lib/libsharp.a _deps/lib/libsharp.a
+ln -s ${PREFIX}/lib/libchealpix.a _deps/lib/libchealpix.a
+
 ${PYTHON} -m pip install . -vv --no-deps
 
 # run tests now since they are not around later
