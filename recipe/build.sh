cf9fdf6e843863c15e4d8a6021b9455cd5feb454
David Alonso <dam.phys@gmail.com>
2021-09-10 17:05:49 +0100

    cleanup

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,19 +2,7 @@
 
 set -e
 
-#if [[ `uname` == 'Darwin' ]]; then
-#    LIBS="-lomp"
-#else
-#    LIBS="-lgomp"
-#fi
-#./configure --prefix=${PREFIX} LIBS="${LIBS}"
-#make
-#make install
-
 mkdir -p _deps/lib
 ln -s ${PREFIX}/lib/libchealpix.a _deps/lib/libchealpix.a
 
 ${PYTHON} -m pip install . -vv --no-deps
-
-# run tests now since they are not around later
-#make check
