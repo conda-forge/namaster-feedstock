106efc24f742bd21cebe85e1a986ed99f6c9f36c
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-05-11 12:10:53 -0500

    Comment out build steps in build.sh
    Comment out directory creation and symlink commands for libchealpix.a.

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,13 +2,12 @@
 
 set -e
 
-mkdir -p _deps/lib
-ln -s ${PREFIX}/lib/libchealpix.a _deps/lib/libchealpix.a
+# mkdir -p _deps/lib
+# ln -s ${PREFIX}/lib/libchealpix.a _deps/lib/libchealpix.a
 
-# try an updated config.*
-if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-  cp $BUILD_PREFIX/share/gnuconfig/config.* .
-fi
+# # try an updated config.*
+# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
+#   cp $BUILD_PREFIX/share/gnuconfig/config.* .
+# fi
 
-# ${PYTHON} -m 
-pip install . -vv --no-deps --no-build-isolation
+${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
