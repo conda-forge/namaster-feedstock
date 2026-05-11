7aabe9dc938db71b71254b0458adf7b7ce5920a8
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-05-11 12:29:57 -0500

    Apply suggestions from code review
    Co-authored-by: Matthew R. Becker <beckermr@users.noreply.github.com>

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,6 @@
 
 set -e
 
-# mkdir -p _deps/lib
-# ln -s ${PREFIX}/lib/libchealpix.a _deps/lib/libchealpix.a
 
-# # try an updated config.*
-# if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" == "1" ]]; then
-#   cp $BUILD_PREFIX/share/gnuconfig/config.* .
-# fi
 
 ${PYTHON} -m pip install . -vv --no-deps --no-build-isolation
